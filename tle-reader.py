59f36fc04b50034700871f2aabb4c2c2950d815c
HighlanderOG <agronmarashaj@gmail.com>
2026-06-08 22:09:20 -0400

    added GMAT api to tle-reader.py

--- tle-reader.py
+++ tle-reader.py
@@ -1,5 +1,20 @@
 from sgp4.api import Satrec, jday
+import os
+import sys
 
+# GMAT placeholder paths (enter the actual paths to your GMAT installation)
+sys.path.append("/home/.../GMAT/R2026a/api")
+sys.path.append("/home/.../GMAT/R2026a/bin")
+os.environ["LD_LIBRARY_PATH"] = "/home/.../GMAT/R2026a/lib"
+
+# GMAT API imports
+import load_gmat  # type: ignore # noqa: E402
+import gmatpy as gmat  # type: ignore # noqa: E402
+
+gmat.Setup("")
+print("GMAT Engine Initialized Successfully!")
+
+# Example TLE data for a satellite (replace with actual TLE data)
 TLE1 = "1 45568U 20025AP  26152.73507564  .09883348  12554-4  24468-3 0  9991"
 TLE2 = "2 45568  53.0178 328.0597 0007364 287.1114  72.9127 16.45563244339443"
 
@@ -8,13 +23,14 @@ satellite = Satrec.twoline2rv(TLE1, TLE2)
 jd = satellite.jdsatepoch
 fr = satellite.jdsatepochF
 
+# Julian Dates
 print(f"Julian Date: {(jd + fr)}")
 print(f"Modded Julian Date: {(jd + fr) - 2400000.5}")
 print(f"GMAT Modified Julian Date: {(jd + fr) - 2430000}")
 
 error, position, velocity = satellite.sgp4(jd, fr)
 
-# POSITION AND VELOCITY VECTORS in TEME FRAME
+# Position and velocity vectors in the TEME frame (km and km/s)
 print(f"Error code: {error}")
 print(f"Position (km): {position}")
 print(f"Velocity (km/s): {velocity}")
