b34387bcd1e6deb296521e8c8b9f5d1138ec9992
HighlanderOG <agronmarashaj@gmail.com>
2026-06-13 23:55:53 +0200

    some changes + coords

--- tle-reader.py
+++ tle-reader.py
@@ -19,9 +19,7 @@ TLE = Satrec.twoline2rv(line1, line2)
 # Other satellite parameters
 drag_coefficient = 2.2
 dry_mass = 450  # kg
-rho = 1e-12  # kg/m^3, very low density for high altitude
-# m^2, using the bstar parameter to estimate the area
-area = TLE.bstar * (2 * dry_mass) / (drag_coefficient * 0.15696615)
+area = TLE.bstar * (2 * dry_mass) / (drag_coefficient * 0.15696615)  # m^2
 
 jd = TLE.jdsatepoch
 fr = TLE.jdsatepochF
@@ -63,19 +61,20 @@ sat.SetField("Cd", drag_coefficient)
 sat.SetField("Cr", 1.8)
 sat.SetField("DragArea", area)
 sat.SetField("SRPArea", area)
+sat.SetField("Id", 'SatId')
+sat.SetField("Attitude", "CoordinateSystemFixed")
 sat.SetField("OrbitErrorCovariance",  (1e+70, 0, 0, 0, 0, 0,
                                        0, 1e+70, 0, 0, 0, 0,
                                        0, 0, 1e+70, 0, 0, 0,
                                        0, 0, 0, 1e+70, 0, 0,
                                        0, 0, 0, 0, 1e+70, 0,
                                        0, 0, 0, 0, 0, 1e+70))
-sat.SetField("Id", 'SatId')
-sat.SetField("Attitude", "CoordinateSystemFixed")
 
-# ForceModels
+# ForceModel
 sat_force_model = gmat.Construct("ForceModel", "SatPropagator_ForceModel")
 sat_force_model.SetField("CentralBody", "Earth")
 
+# Gravity Fields
 earth_gravity = gmat.Construct("GravityField", "Earth10x10")
 earth_gravity.SetField("BodyName", "Earth")
 earth_gravity.SetField(
@@ -85,23 +84,41 @@ earth_gravity.SetField("Order", 10)
 
 moon_gravity = gmat.Construct("GravityField", "MoonGravity")
 moon_gravity.SetField("BodyName", "Luna")
+
 sun_gravity = gmat.Construct("GravityField", "SunGravity")
 sun_gravity.SetField("BodyName", "Sun")
 
+# Atmospheric Drag
 drag_model = gmat.Construct("DragForce", "DragModel")
 drag_model.SetField("AtmosphereModel", "MSISE90")
-
 atmosphere = gmat.Construct("MSISE90", "Atmosphere")
 drag_model.SetReference(atmosphere)
 
+# Solar Radiation Pressure
 srp = gmat.Construct("SolarRadiationPressure", "SRPModel")
 
+# Add forces to the force model
 sat_force_model.AddForce(earth_gravity)
 sat_force_model.AddForce(moon_gravity)
 sat_force_model.AddForce(sun_gravity)
 sat_force_model.AddForce(drag_model)
 sat_force_model.AddForce(srp)
 
-
-# Propagators
+# Propagator
 sat_propagator = gmat.Construct("Propagator", "SatPropagator")
+sat_propagator.SetField("FM", "SatPropagator_ForceModel")
+sat_propagator.SetField("Type", "Runge-Kutta89")
+sat_propagator.SetField("InitialStepSize", 60)
+sat_propagator.SetField("MinStep", 0.001)
+sat_propagator.SetField("MaxStep", 2700)
+sat_propagator.SetField("MaxStepAttempts", 50)
+sat_propagator.SetField("Accuracy", 9.999999999999999e-12)
+sat_propagator.SetField("StopIfAccuracyIsViolated", True)
+
+# Coordinate System
+teme = gmat.Construct("CoordinateSystem", "TEME", "Earth", "TEME")
+
+# Simulation
+gmat.Initialize()
+status = gmat.Execute()
+print(status)
