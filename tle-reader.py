3936e43c30929520d1f1fb5aa5cca8142efe5860
HighlanderOG <agronmarashaj@gmail.com>
2026-06-17 19:43:52 +0200

    forgot to actually change name of report file whoops

--- tle-reader.py
+++ tle-reader.py
@@ -5,7 +5,7 @@ import sys
 # Placeholder paths (enter the actual paths to your GMAT installation and script)
 sys.path.append("/home/.../GMAT/R2026a/api")
 sys.path.append("/home/.../GMAT/R2026a/bin")
-os.environ["LD_LIBRARY_PATH"] = "/home/marashaj/GMAT/GMAT/R2026a/lib"
+os.environ["LD_LIBRARY_PATH"] = "/home/.../GMAT/R2026a/lib"
 script = "/home/.../gmat-spacecraft-decay-analysis/reentry-analysis.script"
 report_file = "/home/.../GMAT/R2026a/output/DecayAnalysisReportFile.txt"
 
